52454cd23772ec5753474fc164e35abdc3c399ba
etibarliparvin <73303441+etibarliparvin@users.noreply.github.com>
2023-05-05 09:09:43 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Test");
+        System.out.println("1Test1");
         System.out.println();
     }
 }
