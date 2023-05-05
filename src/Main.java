86a9ab302a152c4fafd878e8a2370d1189e9b080
etibarliparvin <etibarliparvin@gmail.com>
2023-05-05 09:28:28 +0400

    Added new line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("1Tes1");
+        System.out.println("1st1");
         System.out.println();
     }
 }
